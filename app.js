c58c77fbba9c7d01243a4c8a64cd08f6e9aea496
hainweb <hainofficialweb@gmail.com>
2025-01-22 18:08:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin:['http://localhost:3000'], // Your frontend URL
+  origin:['https://sample-chatapp-fqwv.onrender.com'], // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true  // Allow credentials (cookies) to be sent with requests
